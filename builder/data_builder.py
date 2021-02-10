211fe94e644bdb3967184344c4b77b8ae5601b45
abhigoku10 <abhigoku10@gmail.com>
2021-02-10 09:53:35 +0530

    Update data_builder.py

--- builder/data_builder.py
+++ builder/data_builder.py
@@ -78,10 +78,10 @@ def build_valtest(dataset_config,
     val_imageset = val_dataloader_config["imageset"]
     test_ref = test_dataloader_config["return_ref"]
     val_ref = val_dataloader_config["return_ref"]
-    print('* ',*80)
+    print('* '*80)
     print(test_imageset)
     print(test_imageset)
-    print('* ',*80)
+    print('* '*80)
           
     
 
