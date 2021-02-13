054823208d561d8fe8ffac50cb1a2ec0098038a3
abhigoku10 <abhigoku10@gmail.com>
2021-02-13 14:45:34 +0530

    Update test_cylinder_asym_sk.py

--- test_cylinder_asym_sk.py
+++ test_cylinder_asym_sk.py
@@ -161,7 +161,7 @@ def main(args):
          
             predict_labels = my_model(test_pt_fea_ten, test_grid_ten,test_batch_size)
             
-            #predict_labels = torch.nn.functional.softmax(predict_labels)
+            predict_labels = torch.nn.functional.softmax(predict_labels)
 
             predict_labels = torch.argmax(predict_labels, dim=1)
             predict_labels = predict_labels.cpu().detach().numpy()
