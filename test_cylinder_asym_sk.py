5c241aaec83d9940997e9ed89c0509778702550f
abhigoku10 <abhigoku10@gmail.com>
2021-02-16 21:45:54 +0530

    Update test_cylinder_asym_sk.py

--- test_cylinder_asym_sk.py
+++ test_cylinder_asym_sk.py
@@ -181,13 +181,13 @@ def main(args):
                     except OSError as exc:
                         if exc.errno != errno.EEXIST:
                             raise
-                test_pred_label = test_pred_label.astype(np.uint32)
+#                 test_pred_label = test_pred_label.astype(np.uint32)
 #                 test_pred_label = test_pred_label.astype(np.uint8)
                 upper_half = test_pred_label >> 16  # get upper half for instances
                 lower_half = test_pred_label & 0xFFFF  # get lower half for semantics
 #                 lower_half = remap_lut[lower_half]  # do the remapping of semantics
                 test_pred_label = (upper_half << 16) + lower_half  # reconstruct full label
-                test_pred_label = pred.astype(np.uint32)
+                test_pred_label = test_pred_label.astype(np.uint32)
      
                         
                         
