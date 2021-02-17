0561ea3fb230e709e0aba67b889c6b67cb28c466
abhigoku10 <abhigoku10@gmail.com>
2021-02-17 12:29:53 +0530

    Update test_cylinder_asym_sk.py

--- test_cylinder_asym_sk.py
+++ test_cylinder_asym_sk.py
@@ -194,7 +194,7 @@ def main(args):
 #                 lower_half = test_pred_label & 0xFFFF  # get lower half for semantics
 #                 lower_half = remap_lut[lower_half]  # do the remapping of semantics
 #                 test_pred_label = (upper_half << 16) + lower_half  # reconstruct full label
-#                 test_pred_label = test_pred_label.astype(np.uint32)
+                test_pred_label = test_pred_label.astype(np.uint32)
 #                 print(" The test labels after conversion {}".format(max(test_pred_label, dim=1)))
      
                         
